ee78aea94cf131e8fad358a9f8fc335cea65053e
Peacheap <1ugyuan02@gmail.com>
2025-09-19 14:44:48 +0800

    Build Project

---
+++ src/main/java/com/timberg/gyuan/GyuanApplication.java
@@ -0,0 +1,13 @@
+package com.timberg.gyuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyuanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GyuanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/timberg/gyuan/GyuanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timberg.gyuan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GyuanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
